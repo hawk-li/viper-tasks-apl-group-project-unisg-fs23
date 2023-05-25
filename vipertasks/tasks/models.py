7460545845eb9f749659d126cfefa78bc44320b4
dfalk <damian.falk@student.unisg.ch>
2023-05-25 17:33:36 +0200

    migrate

--- vipertasks/tasks/models.py
+++ vipertasks/tasks/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-    date_created = models.DateTimeField('date created')
-    date_due = models.DateTimeField('date due')
-    date_completed = models.DateTimeField('date completed')
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_due = models.DateTimeField('date due', null=True)
+    date_completed = models.DateTimeField('date completed', null=True)
     completed = models.BooleanField(default=False)
     def __str__(self):
         return self.name
