98827111b6121a4c0340221da400b079ac944508
dfalk <damian.falk@student.unisg.ch>
2023-05-25 17:31:09 +0200

    first templates

--- vipertasks/tasks/urls.py
+++ vipertasks/tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<str:user_name>/tasks', views.IndexView.as_view(), name='user'),
 ]
--- vipertasks/tasks/views.py
+++ vipertasks/tasks/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 
+from .models import Task
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, APL")
+    # return template from tasks/index.html
+    return render(request, 'tasks/index.html')
 
 class IndexView(generic.ListView):
-    template_name = 'tasks/index.html'
+    template_name = 'tasks/tasklist.html'
     context_object_name = 'task_list'
+    queryset = Task.objects.all()
     
